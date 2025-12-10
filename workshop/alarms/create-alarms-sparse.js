9e2cb1c9cede8865686b0902bdc15ca3e5431131
Jakub Stanisławczyk <jakubstanislawczyk98@gmail.com>
2025-12-10 13:34:01 +0100

    Alarms

---
+++ workshop/alarms/create-alarms-sparse.js
@@ -0,0 +1,55 @@
+import { initDDBClient } from "../common/init-ddb-client.js";
+import { BatchWriteCommand } from "@aws-sdk/lib-dynamodb";
+import { resourcePrefix } from "../consts.js";
+
+const buildEvent = (sensorId, value, createdAt, status) => {
+  return {
+    id: crypto.randomUUID(),
+    sensorId,
+    value,
+    createdAt,
+    status,
+    ALARM_GSI_PK: status === "ALARM" ? sensorId : undefined,
+  };
+};
+
+const buildEvents = () => {
+  return [
+    // Events for sensor-1
+    buildEvent("1111", "sensor-1", 23.0, 1756068060, "OK"),
+    buildEvent("2222", "sensor-1", 25.5, 1756068120, "OK"),
+    buildEvent("3333", "sensor-1", 30.2, 1756068180, "ALARM "),
+    buildEvent("4444", "sensor-1", 28.4, 1756068240, "ALARM"),
+    // Events for sensor-2
+    buildEvent("5555", "sensor-2", 31.5, 1756068120, "ALARM"),
+  ];
+};
+
+const saveEvents = async (sensorEvents) => {
+  const ddbClient = initDDBClient();
+  const saveEventsRequests = sensorEvents.map((event) => ({
+    PutRequest: {
+      Item: event,
+    },
+  }));
+  const command = new BatchWriteCommand({
+    RequestItems: {
+      [`${resourcePrefix}-alarms-sparse`]: saveEventsRequests,
+    },
+  });
+
+  try {
+    await ddbClient.send(command);
+
+    const ids = sensorEvents.map((e) => e.id).join(", ");
+    console.log("Events saved successfully. IDs: ", ids);
+  } catch (error) {
+    console.error("Error saving events:", error);
+    throw error;
+  }
+};
+
+(async () => {
+  const events = buildEvents();
+  await saveEvents(events);
+})();
